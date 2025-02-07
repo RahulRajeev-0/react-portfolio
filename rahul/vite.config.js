1700de0a2fcf861a57ba8ad0a91ea981fa0b57dd
Rahul Rajeev <rahulrajeev836@gmail.com>
2025-02-07 14:13:42 +0530

    update vite config

--- rahul/vite.config.js
+++ rahul/vite.config.js
@@ -1,10 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
-})
+  preview: {
+    port: process.env.PORT || 4173, // Use Render's assigned port
+    host: "0.0.0.0", // Allow external connections
+  },
+});
